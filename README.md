a02f1bc9c9226001e48022317be10833b768d174
Peter Ongyert <peter.ongyert@medienworx.eu>
2015-03-12 14:57:34 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# mwk-helper-state
+## Bundesländer aus einer Lang-Datei laden (DE, AT, CH)
+===============================
+
+### Dokumentation
+-----------------------------
+
+**Anleitung:**
+
+In deiner DCA-Datei oberhalb von ```$GLOBALS ['TL_DCA]...``` wird der Pfad zur Lang-Datei ```\System::loadLanguageFile('states');``` eingefügt:
+
+
+z.B Bei einem **SELECT Menü** im DCA-File unter Fields bei ```'options' ```
+wird als Wert ```&$GLOBALS['TL_LANG']['STATE'],``` (alle Bundesländer werden geladen) oder
+```&$GLOBALS['TL_LANG']['STATE']['DE'],``` (Bundesländer von Deutschland werden geladen) oder
+```&$GLOBALS['TL_LANG']['STATE']['AT'],``` (Bundesländer von Österreich werden geladen) oder
+```&$GLOBALS['TL_LANG']['STATE']['AT'],``` (Kantons von der Schweiz werden geladen)  eingebunden,
+die Bundesländer werden jetzt über die Lang-Datei geladen.
+
+
+BEISPIEL:
+```
+'name' => array
+		(
+			'label'                 => &$GLOBALS['TL_LANG']['tabellenname']['name'],
+			'inputType'             => 'select',
+			'options'               => &$GLOBALS['TL_LANG']['STATE'],
+			'eval'                  => array
+			(
+				'includeBlankOption'=>true,
+				'mandatory'=>true,
+				'chosen'=>true,
+				'tl_class'=>'w50',
+			),
+			'sql'                   => "varchar(3) NOT NULL default ''",
+		),
+```
+
+-----------------------------
+
+### Systeminformationen
+-----------------------------
+
+Contao 3.2.x oder höher.
+
+-----------------------------
+
+Lizenz: LGPL
---
+++ system/modules/mwk-helper-state/languages/en/state.php
@@ -0,0 +1,71 @@
+<?php
+ 
+ /**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2015 - 2015 Agentur medienworx
+ *
+ * @package     mwk-helper-CUR_SHORTrency
+ * @author      Christian Kienzl <christian.kienzl@medienworx.eu>
+ * @author      Peter Ongyert <peter.ongyert@medienworx.eu>
+ * @link        http://www.medienworx.eu
+ * @license     http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+// Bundesländer in DE
+$GLOBALS['TL_LANG']['STATE']['DE']['BW'] = 'Baden-Württemberg';
+$GLOBALS['TL_LANG']['STATE']['DE']['BY'] = 'Bayern';
+$GLOBALS['TL_LANG']['STATE']['DE']['BB'] = 'Berlin';
+$GLOBALS['TL_LANG']['STATE']['DE']['BB'] = 'Brandenburg';
+$GLOBALS['TL_LANG']['STATE']['DE']['HH'] = 'Hamburg';
+$GLOBALS['TL_LANG']['STATE']['DE']['HE'] = 'Hessen';
+$GLOBALS['TL_LANG']['STATE']['DE']['MV'] = 'Mecklenburg-Vorpommern';
+$GLOBALS['TL_LANG']['STATE']['DE']['NI'] = 'Niedersachsen';
+$GLOBALS['TL_LANG']['STATE']['DE']['NW'] = 'Nordrhein-Westfalen';
+$GLOBALS['TL_LANG']['STATE']['DE']['RP'] = 'Rheinland-Pfalz';
+$GLOBALS['TL_LANG']['STATE']['DE']['SL'] = 'Saarland';
+$GLOBALS['TL_LANG']['STATE']['DE']['SN'] = 'Sachsen';
+$GLOBALS['TL_LANG']['STATE']['DE']['ST'] = 'Sachsen-Anhalt';
+$GLOBALS['TL_LANG']['STATE']['DE']['SH'] = 'Schleswig-Holstein';
+$GLOBALS['TL_LANG']['STATE']['DE']['TH'] = 'Thüringen';
+
+// Bundesländer in AT
+$GLOBALS['TL_LANG']['STATE']['AT']['B'] = 'Burgenland';
+$GLOBALS['TL_LANG']['STATE']['AT']['K'] = 'Kärnten';
+$GLOBALS['TL_LANG']['STATE']['AT']['NÖ'] = 'Niederösterreich';
+$GLOBALS['TL_LANG']['STATE']['AT']['OÖ'] = 'Oberösterreich';
+$GLOBALS['TL_LANG']['STATE']['AT']['S'] = 'Salzburg';
+$GLOBALS['TL_LANG']['STATE']['AT']['ST'] = 'Steiermark';
+$GLOBALS['TL_LANG']['STATE']['AT']['T'] = 'Tirol';
+$GLOBALS['TL_LANG']['STATE']['AT']['V'] = 'Vorarlberg';
+$GLOBALS['TL_LANG']['STATE']['AT']['W'] = 'Wien';
+
+// Kanton in CH
+$GLOBALS['TL_LANG']['STATE']['CH']['ZH'] = 'Zürich';
+$GLOBALS['TL_LANG']['STATE']['CH']['BE'] = 'Bern';
+$GLOBALS['TL_LANG']['STATE']['CH']['LU'] = 'Luzern';
+$GLOBALS['TL_LANG']['STATE']['CH']['UR'] = 'Uri';
+$GLOBALS['TL_LANG']['STATE']['CH']['SZ'] = 'Schwyz';
+$GLOBALS['TL_LANG']['STATE']['CH']['OW'] = 'Obwalden';
+$GLOBALS['TL_LANG']['STATE']['CH']['NW'] = 'Nidwalden';
+$GLOBALS['TL_LANG']['STATE']['CH']['GL'] = 'Glarus';
+$GLOBALS['TL_LANG']['STATE']['CH']['ZG'] = 'Zug';
+$GLOBALS['TL_LANG']['STATE']['CH']['FR'] = 'Freiburg';
+$GLOBALS['TL_LANG']['STATE']['CH']['SO'] = 'Solothurn';
+$GLOBALS['TL_LANG']['STATE']['CH']['BS'] = 'Basel-Stadt';
+$GLOBALS['TL_LANG']['STATE']['CH']['BL'] = 'Basel-Landschaft';
+$GLOBALS['TL_LANG']['STATE']['CH']['SH'] = 'Schaffhausen';
+$GLOBALS['TL_LANG']['STATE']['CH']['AR'] = 'Appenzell Ausserrhoden';
+$GLOBALS['TL_LANG']['STATE']['CH']['AI'] = 'Appenzell Innerrhoden';
+$GLOBALS['TL_LANG']['STATE']['CH']['SG'] = 'St. Gallen';
+$GLOBALS['TL_LANG']['STATE']['CH']['GR'] = 'Graubünden';
+$GLOBALS['TL_LANG']['STATE']['CH']['AG'] = 'Aargau';
+$GLOBALS['TL_LANG']['STATE']['CH']['TG'] = 'Thurgau';
+$GLOBALS['TL_LANG']['STATE']['CH']['TI'] = 'Tessin';
+$GLOBALS['TL_LANG']['STATE']['CH']['VD'] = 'Waadt';
+$GLOBALS['TL_LANG']['STATE']['CH']['VS'] = 'Wallis';
+$GLOBALS['TL_LANG']['STATE']['CH']['NE'] = 'Neuenburg';
+$GLOBALS['TL_LANG']['STATE']['CH']['GE'] = 'Genf';
+$GLOBALS['TL_LANG']['STATE']['CH']['JU'] = 'Jura';
+
+
